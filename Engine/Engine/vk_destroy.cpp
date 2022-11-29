e9fbdde645031b2ad028c7d94de082fa07cba393
romain <romain.crespel@gmail.com>
2022-11-29 07:46:19 +0100

    merge

--- Engine/Engine/vk_destroy.cpp
+++ Engine/Engine/vk_destroy.cpp
@@ -27,8 +27,6 @@ namespace Vulkan
 		vkFreeMemory(a_logicalDevice, a_toDestroy.memory, nullptr);
 	}
 
-<<<<<<< HEAD
-
 	void destroyVulkanInstance(VkInstance& a_vkInstance)
 	{
 		vkDestroyInstance(a_vkInstance, nullptr);
@@ -39,11 +37,9 @@ namespace Vulkan
 		vkDestroyDevice(a_device.logicalDevice, nullptr);
 	}
 
-=======
 	void destroyBuffer(VkDevice a_logicalDevice, Buffer& a_toDestroy)
 	{
 		vkDestroyBuffer(a_logicalDevice, a_toDestroy.buffer, nullptr);
 		vkFreeMemory(a_logicalDevice, a_toDestroy.memory, nullptr);
 	}
->>>>>>> a597d2f6d4acdf8406486f7f9bbdfd6cc68f8272
 }
