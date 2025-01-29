a0f553b3f1a4f915821b545214e9a3a7349145c5
roomain <romain.crespel@gmail.com>
2025-01-29 08:00:37 +0100

    transformation

--- Engine/math/transformation.h
+++ Engine/math/transformation.h
@@ -32,25 +32,58 @@ public:
 	/*@brief rotate around a point with axis*/
 	inline void rotateAround(const glm::dvec3& a_center, const glm::dvec3& a_axis, const double& a_radianAngle)
 	{
-		//glm::rotate()
-		// todo
+		m_transformMatrix = glm::translate(m_transformMatrix, -a_center);
+		m_transformMatrix = glm::rotate(m_transformMatrix, a_radianAngle, a_axis);
+		m_transformMatrix = glm::translate(m_transformMatrix, a_center);
+	}
+
+	inline void reset()
+	{
+		m_transformMatrix = glm::identity<glm::dmat4>();
 	}
 
 	/*@brief self rotate*/
 	inline void selfRotate(const glm::dvec3& a_axis, const double& a_radianAngle)
 	{
-		//todo
+		rotateAround(getPosition(), a_axis, a_radianAngle);
 	}
 
 	/*@brief set position*/
 	inline void setPosition(const glm::dvec3& a_position)
 	{
-		//todo
+		m_transformMatrix[3] = glm::dvec4(a_position, 1);
 	}
 
 	/*@brief translate*/
 	inline void translate(const glm::dvec3& a_translation)
 	{
-		//todo
+		m_transformMatrix = glm::translate(m_transformMatrix, a_translation);
+	}
+
+	inline void scale(const glm::dvec3& a_scale)
+	{
+		m_transformMatrix = glm::scale(m_transformMatrix, a_scale);
+	}
+
+	inline void setScale(const glm::dvec3& a_scale)
+	{
+		m_transformMatrix[0][0] = a_scale.x;
+		m_transformMatrix[1][1] = a_scale.y;
+		m_transformMatrix[2][2] = a_scale.z;
+	}
+
+	constexpr glm::dvec3 getPosition()const
+	{
+		return m_transformMatrix[3];
+	}
+
+	constexpr glm::dvec3 getScale()const
+	{
+		return glm::dvec3(m_transformMatrix[0][0], m_transformMatrix[1][1], m_transformMatrix[2][2]);
+	}
+
+	constexpr glm::dquat getOrientation()const
+	{
+		return glm::quat_cast(m_transformMatrix);
 	}
 };
