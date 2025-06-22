833e2b40ba0d8c4cb9e76b88c51e2026fe2a7f6e
roomain <romain.crespel@gmail.com>
2025-06-22 10:00:03 +0200

    context

--- Engine/EngineRenderingContext.h
+++ Engine/EngineRenderingContext.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <type_traits>
 #include <memory>
 #include <vulkan/vulkan.hpp>
 class VulkanPipeline;
@@ -9,4 +10,19 @@ class VulkanPipeline;
 struct EngineRenderingContext
 {
 	std::unique_ptr<VulkanPipeline> m_pipeline;	/*!< pipeline for specific object category*/
+	bool m_isPipelineBound = false;				/*!< indicate if pipeline is bound in current command buffer*/
+};
+
+
+template<typename Context> requires(std::is_base_of_v<EngineRenderingContext, Context>)
+class EngineDrawable
+{
+	friend class EngineRenderer;
+
+protected:
+	static inline Context s_context;
+
+public:
+	/*@brief render the object*/
+	virtual void render(VkCommandBuffer a_commandBuffer) = 0;
 };
